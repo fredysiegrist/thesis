104fa7efa5485d67f55ecdb992d2dbffa5c5890a
fredysiegrist <fredy.siegrist@students.unibe.ch>
2016-08-11 08:06:04 +0200

    genetic code table

---
+++ master-thesis/codes/geneticcode.R
@@ -0,0 +1,5 @@
+require(seqinr)
+tablecode()
+# to plot a codon table with ratios and frequencies with the overwriting function from statanacoseq
+require(statanacoseq)
+tablecode(mylist(whatout=1))
