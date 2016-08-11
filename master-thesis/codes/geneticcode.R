f78cdaccaab4352c169fcbe7af7e5d09a465ce0d
fredysiegrist <fredy.siegrist@students.unibe.ch>
2016-08-11 08:25:37 +0200

    genetic code as table not image

--- master-thesis/codes/geneticcode.R
+++ master-thesis/codes/geneticcode.R
@@ -1,5 +1,5 @@
 require(seqinr)
-tablecode()
+tablecode(latexfile="codontable.tex")
 # to plot a codon table with ratios and frequencies with the overwriting function from statanacoseq
 require(statanacoseq)
 tablecode(mylist(whatout=1))
