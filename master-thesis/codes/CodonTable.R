e650e9474143bfbfd25e1a6c8e1e69b2dd351b57
fredysiegrist <fredy.siegrist@students.unibe.ch>
2016-08-17 20:38:37 +0200

    end of day

--- master-thesis/codes/CodonTable.R
+++ master-thesis/codes/CodonTable.R
@@ -1,37 +1,25 @@
 require(statanacoseq)
 require(seqinr)
 verified <- mylist("/windows/R/Round3.all.maker.transcripts.verified.fasta", "/windows/R/Round3.all.maker.proteins.verified.fasta", whatout=1)
-#CodonUsage(unlist(verified))
-#uco(unlist(verified), 0, "eff")
-tabl <- uco(verified[[1]], 1, "eff")
-for (n in 2:length(verified)) { 
- tabl <- tabl + uco(verified[[n]], 1, "eff")
-}
-
-tabl <- uco(verified[[1]], 1, "eff")
+# the process is killed when using to many sequences, so it is divided in four parts and added together later
+tabl <- uco(verified[[1]], 0, "eff")
 for (n in 2:10000) { #length(verified)
- tabl <- tabl + uco(verified[[n]], 1, "eff")
+ tabl <- tabl + uco(verified[[n]], 0, "eff")
 }
-
-
-tabl1 <- uco(verified[[10001]], 1, "eff")
+tabl1 <- uco(verified[[10001]], 0, "eff")
 for (n in 10002:20000) { 
- tabl <- tabl + uco(verified[[n]], 1, "eff")
+ tabl1 <- tabl + uco(verified[[n]], 0, "eff")
 }
-
-tabl2 <- uco(verified[[20001]], 1, "eff")
+tabl2 <- uco(verified[[20001]], 0, "eff")
 for (n in 20002:30000) { 
- tabl <- tabl + uco(verified[[n]], 1, "eff")
+ tabl2 <- tabl + uco(verified[[n]], 0, "eff")
 }
-
-tabl3 <- uco(verified[[30001]], 1, "eff")
+tabl3 <- uco(verified[[30001]], 0, "eff")
 for (n in 30002:41988) { 
- tabl <- tabl + uco(verified[[n]], 1, "eff")
+ tabl3 <- tabl + uco(verified[[n]], 0, "eff")
 }
-
 tabla <- tabl+tabl1+tabl2+tabl3
 perc <- round(tabla/nuu*100,2)
-
-
+(rbind(tabla, perc))
 
 
